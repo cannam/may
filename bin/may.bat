956b577cc686816f6e87e5e874019614e94c8867
Chris Cannam <c.cannam@qmul.ac.uk>
2013-09-05 21:52:21 +0100

    Add Ant build task and Windows batch file

---
+++ bin/may.bat
@@ -0,0 +1,1 @@
+java -classpath "may.jar;ext\jar\ayr.jar;..\yeti\yeti.jar;ext\jar\jline-2.11-SNAPSHOT.jar;ext\jar\jvamp.jar;ext\jar\yertle.jar;ext\jar\jtransforms-2.4.jar;ext\jar\org.jzy3d-0.9.jar;ext\jar\jogl-all.jar;ext\jar\gluegen.jar;ext\jar\opencsv-2.1.jar;ext\jar\org.convexhull.jar" -Djava.library.path=ext\native\win64 com.particularprograms.ayr
